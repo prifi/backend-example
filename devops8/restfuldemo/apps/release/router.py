09becc90f407eb72f299119ede0b840372635e36
Prifi <694748391@qq.com>
2022-12-18 20:43:10 +0800

    A devops8

--- devops8/restfuldemo/apps/release/router.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import DeployViewset
-
-deploy_router = DefaultRouter()
-deploy_router.register(r'deploy', DeployViewset, base_name="deploy")
--- devops8/restfuldemo/apps/workorder/router.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from .views import WorkOrderViewset
-
-workorder_router = DefaultRouter()
-workorder_router.register(r'workorder', WorkOrderViewset, base_name="workorder")
